0b2f8ef183c7640567b2c239f85e4c713846e43b
Evan Sciancalepore <evanscianc@gmail.com>
2025-10-22 15:40:53 -0700

    Challenge #2 - using two for loops to render the board

--- src/App.js
+++ src/App.js
@@ -33,24 +33,28 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next move belongs to " + (xIsNext ? "X" : "O")
   }
 
+  // Two for loops - my solution, but not as idiomatic as using .map(), which we'll do next
+  const rows = []
+  for (let r = 0; r < 3; r++) {
+    const rowSquares = []
+    for (let c = 0; c < 3; c++) {
+      const index = r * 3 + c
+      rowSquares.push((
+        <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />
+      ))
+    }
+
+    rows.push((
+      <div key={r} className="board-row">
+        {rowSquares}
+      </div>
+    ))
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {rows}
     </>
   )
 }
